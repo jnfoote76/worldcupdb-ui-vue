607a19edf0f6f72fd310c290b2c80b4d57d2f4cb
nathaniel <nathaniel@localhost.localdomain>
2017-03-24 21:38:18 -0400

    Fixed mistake durng previous renaming

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/index'
+import Index from '@/components/index'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'index',
-      component: Hello
+      name: 'Index',
+      component: Index
     }
   ]
 })
